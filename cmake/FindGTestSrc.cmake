733b7de48234db2d4bf622e0882a1b7cdb74e674
Muhammad Nawaz <m.nawaz2003@gmail.com>
2024-06-15 20:18:31 +0500

    Fix for apple clang build issue for GTest

--- cmake/FindGTestSrc.cmake
+++ cmake/FindGTestSrc.cmake
@@ -1,26 +1,27 @@
-
 SET(GTEST_SEARCH_PATH
-    "${GTEST_SOURCE_DIR}"
-    "${CMAKE_CURRENT_LIST_DIR}/../thirdparty/googletest/googletest")
+        "${CMAKE_CURRENT_LIST_DIR}/../thirdparty/googletest/googletest"
+        "${GTEST_SOURCE_DIR}"
+)
 
 IF(UNIX)
     LIST(APPEND GTEST_SEARCH_PATH "/usr/src/gtest")
 ENDIF()
 
 FIND_PATH(GTEST_SOURCE_DIR
-    NAMES CMakeLists.txt src/gtest_main.cc
-    PATHS ${GTEST_SEARCH_PATH})
-
+        NAMES CMakeLists.txt src/gtest_main.cc
+        PATHS ${GTEST_SEARCH_PATH}
+        NO_DEFAULT_PATH)
 
 # Debian installs gtest include directory in /usr/include, thus need to look
 # for include directory separately from source directory.
 FIND_PATH(GTEST_INCLUDE_DIR
-    NAMES gtest/gtest.h
-    PATH_SUFFIXES include
-    HINTS ${GTEST_SOURCE_DIR}
-    PATHS ${GTEST_SEARCH_PATH})
+        NAMES gtest/gtest.h
+        PATH_SUFFIXES include
+        HINTS ${GTEST_SOURCE_DIR}
+        PATHS ${GTEST_SEARCH_PATH}
+        NO_DEFAULT_PATH)
 
 INCLUDE(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GTestSrc DEFAULT_MSG
-    GTEST_SOURCE_DIR
-    GTEST_INCLUDE_DIR)
+        GTEST_SOURCE_DIR
+        GTEST_INCLUDE_DIR)
